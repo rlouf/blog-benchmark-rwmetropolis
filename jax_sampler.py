fix sign of loglikelihood

jax_sampler.py
@@ -91,7 +91,7 @@ def mixture_logpdf(x):
     dist_4 = jax.partial(norm.logpdf, loc=2.5, scale=2.8)
     log_probs = np.array([dist_1(x), dist_2(x), dist_3(x), dist_4(x)])
     weights = np.array([0.2, 0.3, 0.1, 0.4])
-    return -logsumexp(np.log(weights) + log_probs)
+    return logsumexp(np.log(weights) + log_probs)
 
 
 if __name__ == "__main__":

pytorch_sampler.py
@@ -61,7 +61,7 @@ def mixture_logpdf(x):
     """
     log_probs = normal_loc_scale.log_prob(x)
 
-    return -torch.logsumexp(torch.log(weights) + log_probs, axis=0)
+    return torch.logsumexp(torch.log(weights) + log_probs, axis=0)
 
 
 if __name__ == "__main__":

pytorch_sampler_gpu.py
@@ -31,7 +31,6 @@ def rw_metropolis_sampler(logpdf, initial_position):
         proposal = position + move_proposals
         proposal_log_prob = logpdf(proposal)
 
-        normal = ()
         log_uniform = torch.log(torch.rand(
             initial_position.shape[0], initial_position.shape[1],
             device=DEVICE))
@@ -64,7 +63,7 @@ def mixture_logpdf(x):
     """
     log_probs = normal_loc_scale.log_prob(x)
 
-    return -torch.logsumexp(torch.log(weights) + log_probs, axis=0)
+    return torch.logsumexp(torch.log(weights) + log_probs, axis=0)
 
 
 if __name__ == "__main__":